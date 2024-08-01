d3bead004ad73855c213648d7b86c4fc9084321c
Azim Baghadiya <azimsb120@gmail.com>
2024-08-01 01:19:28 -0400

    chore: enable auto formatting with black

--- api/src/config.py
+++ api/src/config.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class Config:
-    ENVIRONMENT = os.getenv('ENVIRONMENT', 'local')
-    if ENVIRONMENT == 'docker':
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI_DOCKER')
+    ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+    if ENVIRONMENT == "docker":
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI_DOCKER")
     else:
-        SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI_LOCAL')
+        SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI_LOCAL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
